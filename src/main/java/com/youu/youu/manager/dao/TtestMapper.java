e5df7b8dc8de7b7ee2aac62e429460ec46c7749b
ThrumanOne <1006556989@qq.com>
2017-08-10 16:58:11 +0800

    添加通用mapper

--- src/main/java/com/youu/youu/manager/dao/TtestMapper.java
+++ src/main/java/com/youu/youu/manager/dao/TtestMapper.java
@@ -1,11 +1,14 @@
 package com.youu.youu.manager.dao;
 
+import com.github.abel533.mapper.Mapper;
 import com.youu.youu.manager.bean.Ttest;
 import com.youu.youu.manager.bean.TtestExample;
+
 import java.util.List;
+
 import org.apache.ibatis.annotations.Param;
 
-public interface TtestMapper {
+public interface TtestMapper extends Mapper<Ttest> {
     long countByExample(TtestExample example);
 
     int deleteByExample(TtestExample example);
