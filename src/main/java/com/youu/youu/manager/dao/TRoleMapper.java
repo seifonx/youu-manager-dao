00a3e1432e70f2e246ed786e4daeaa6ab00757df
ThrumanOne <1006556989@qq.com>
2017-08-11 19:02:07 +0800

    角色验证

--- src/main/java/com/youu/youu/manager/dao/TRoleMapper.java
+++ src/main/java/com/youu/youu/manager/dao/TRoleMapper.java
@@ -3,7 +3,14 @@ package com.youu.youu.manager.dao;
 import com.github.abel533.mapper.Mapper;
 import com.youu.youu.manager.bean.TRole;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public interface TRoleMapper extends Mapper<TRole> {
+
+	HashSet<String> selectRoleByManagerId(String manager);
+	
+	
+	
 }
